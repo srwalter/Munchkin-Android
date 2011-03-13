2affe5eb9f0e90943b09b3c612fe1e66eb57ef0d
Chris Slinkman <Chris.Slinkman@gmail.com>
2011-03-13 14:38:49 -0500

    Skelton for Fight Presenter Testing

---
+++ MunchkinPresenters/src/test/java/com/slinkman/munchkin/mocks/view/FightViewMock.java
@@ -0,0 +1,7 @@
+package com.slinkman.munchkin.mocks.view;
+
+import com.slinkman.munchkin.apis.view.FightView;
+
+public class FightViewMock implements FightView {
+
+}
---
+++ MunchkinPresenters/src/test/java/com/slinkman/munchkin/presenter/FightTest.java
@@ -0,0 +1,13 @@
+package com.slinkman.munchkin.presenter;
+
+import org.junit.Before;
+
+public class FightTest {
+
+	
+	
+	@Before
+	public void setup(){
+		
+	}
+}
